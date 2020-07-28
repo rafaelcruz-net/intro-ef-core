7a58671514e714ea29cdecc45a0bc01f11f58b96
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2020-07-28 09:13:18 -0300

    Adicionando log no EF core

--- Repository/Context/InfnetContext.cs
+++ Repository/Context/InfnetContext.cs
@@ -1,5 +1,6 @@
 ﻿using Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Repository.Mapeamento;
 using System;
 using System.Collections.Generic;
@@ -10,10 +11,21 @@ namespace Repository.Context
     public class InfnetContext : DbContext
     {
         public DbSet<Aluno> Alunos { get; set; }
-        
+
+        public static readonly ILoggerFactory _loggerFactory 
+            = LoggerFactory.Create(builder => { builder.AddConsole(); });
+
+
         public InfnetContext(DbContextOptions<InfnetContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseLoggerFactory(_loggerFactory);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
